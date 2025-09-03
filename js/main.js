51f62008a7a84c40978b377a0b955b0bcd8f4f29
nasrinmgh <nasrinmohaghegh98@gmail.com>
2025-09-03 11:11:31 +0200

    styles/Quote API set

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
 const API_KEY = 'hBCMhnX75QQf4kzkxP3etQ==vtDSjzQckETg3IKO';
 
 async function getNewQuote() {
-    
+  let quote = "";
+  let author = "";
+
+  // Keep fetching until we find a short quote
+  while (true) {
     const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
-    headers: {
-      "X-Api-Key": API_KEY
+      headers: {
+        "X-Api-Key": API_KEY
+      }
+    });
+
+    const data = await response.json();
+    quote = data[0].quote;
+    author = data[0].author;
+
+    // If quote length <= 50, stop loop
+    if (quote.length <= 50) {
+      break;
     }
-  });
-   const data = await response.json();
-   console.log(data);
-   const quote = data[0].quote;
-   const  author = data[0].author;
- 
-   const quoteSpan = document.getElementById('quoteSpan');
-   const authorSpan = document.getElementById('authorSpan');
-   quoteSpan.textContent = `${quote}`;
-   authorSpan.textContent = `${author}`;
+  }
+
+  // Update UI
+  document.getElementById("quote_p").textContent = `"${quote}"`;
+  document.getElementById("authorSpan").textContent = `— ${author}`;
 }
 
-const newBtn = document.getElementById('getNewQuote');
-newBtn.addEventListener('click', getNewQuote());
+const newBtn = document.getElementById("getNewQuote");
+newBtn.addEventListener("click", getNewQuote);
 
+// Load first quote on page load
 getNewQuote();
 
-
+// To copy the quote
+const copyBtn = document.getElementById('copy');
+copyBtn.addEventListener('click', () => {
+  quote = document.getElementById()
+})
 
