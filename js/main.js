ebca19aa7fbf315ee641e16e1131aa2c01006006
nasrinmgh <nasrinmohaghegh98@gmail.com>
2025-09-04 15:48:16 +0200

    Toggle logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-const API_KEY = 'hBCMhnX75QQf4kzkxP3etQ==vtDSjzQckETg3IKO';
+// Toggle Mode
 
+function toggleMode() {
+  const dark = document.getElementById("toggle");
+  const card = document.querySelector(".card");
+
+  dark.addEventListener("change", () => {
+    if (dark.checked) {
+      card.classList.add("dark-mode");
+    } else {
+      card.classList.remove("dark-mode");
+    }
+  });
+}
+toggleMode();
+
+const API_KEY = "hBCMhnX75QQf4kzkxP3etQ==vtDSjzQckETg3IKO";
 async function getNewQuote() {
   let quote = "";
   let author = "";
@@ -8,8 +23,8 @@ async function getNewQuote() {
   while (true) {
     const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
       headers: {
-        "X-Api-Key": API_KEY
-      }
+        "X-Api-Key": API_KEY,
+      },
     });
 
     const data = await response.json();
@@ -32,32 +47,34 @@ newBtn.addEventListener("click", getNewQuote);
 
 // Load first quote on page load
 getNewQuote();
- 
+
 // To copy the quote
 async function copyQuote() {
-  try{
-  const quoteText = document.querySelector('.card').textContent;
-  quoteText.trim();
-  await navigator.clipboard.writeText(quoteText);
-
-  } catch(error) {
+  const quoteText = document.querySelector(".card").textContent;
+  if (quoteText.trim() === "") return false;
+  try {
+    await navigator.clipboard.writeText(quoteText.trim());
+    return true;
+  } catch (error) {
     console.log(error);
+    return false;
   }
 }
 
-const copyBtn = document.getElementById('copy');
-copyBtn.addEventListener('click', () => {
- copyQuote();
- if (copyQuote) {
-  const copyAlert = document.createElement('span');
-  copyAlert.classList.add('copy-alert');
-  copyAlert.textContent = 'Copied successfully!';
-  const container = document.querySelector('.container');
+const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", async () => {
+  const ok = await copyQuote();
+  const copyAlert = document.createElement("span");
+  const container = document.querySelector(".container");
   container.appendChild(copyAlert);
+  copyAlert.classList.add("copy-alert");
 
- setTimeout(() => {
-  container.removeChild(copyAlert);
- }, 1000)
+  if (ok) {
+    copyAlert.textContent = "Copied successfully!";
+  } else {
+    copyAlert.textContent = "Nothing to copy!";
   }
-
-})
+  setTimeout(() => {
+    container.removeChild(copyAlert);
+  }, 1000);
+});
